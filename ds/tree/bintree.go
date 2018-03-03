4f58fe516068685a209ddb842e0a852de08ce350
j0gger <sachin.manpathak@gmail.com>
2018-03-02 16:56:38 -0800

    Add isBalanced

--- ds/tree/bintree.go
+++ ds/tree/bintree.go
@@ -5,6 +5,10 @@
 // bintree package implements binary tree data structure
 package bintree
 
+import (
+	"errors"
+)
+
 type Node struct {
 	val int
 	left *Node
@@ -38,9 +42,62 @@ func _Inorder(root *Node, values *[]int) {
 	_Inorder(root.right, values)
 }
 
-func InorderTraversal(root *Node) []int {
+func TreeToInorder(root *Node) []int {
 	values := make([]int, 0)
 	_Inorder(root, &values)
 
 	return values
+}
+
+func GetSmallerNeighbor(root *Node, val int) (*Node,  error) {
+	if root == nil {
+		return root, errors.New("Not found")
+	}
+	
+	if root.val <= val {
+		return root, nil
+	}
+
+	return GetSmallerNeighbor(root.left, val)
+}
+
+func _Abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+
+	return a
+}
+
+func _Max(a int, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func _IsBalanced(root *Node) (bool, int) {
+	if root == nil {
+		return true, 0
+	}
+
+	leftOk, leftHeight := _IsBalanced(root.left)
+
+	if leftOk == false {
+		return false, 0
+	}
+
+	rightOk, rightHeight := _IsBalanced(root.right)
+
+	if rightOk == false {
+		return false, 0
+	}
+
+	return _Abs(leftHeight - rightHeight) <= 1, _Max(leftHeight, rightHeight) + 1
+}
+
+func IsBalanced(root *Node) bool {
+	isBalanced, _ := _IsBalanced(root)
+	return isBalanced
 }
--- ds/tree/bintree_test.go
+++ ds/tree/bintree_test.go
@@ -20,7 +20,38 @@ func TestBuild(t *testing.T) {
 	}
 
 	sort.Ints(vals)
-	sortedVals := InorderTraversal(root)
+	sortedVals := TreeToInorder(root)
 	assert.Equal(t, vals, sortedVals)
 	
+}
+
+func TestUnBalanced(t *testing.T) {
+	vals := []int{67, 32, 11, 0, 3, 12, 16, 42, 39}
+	var root *Node
+	for _, v := range vals {
+		root = Insert(root, v)
+	}
+
+	assert.Equal(t, false, IsBalanced(root))
+}
+
+func TestUnBalanced2(t *testing.T) {
+	vals := []int{1, 2, 3, 4, 5, 6}
+	var root *Node
+	for _, v := range vals {
+		root = Insert(root, v)
+	}
+
+	assert.Equal(t, false, IsBalanced(root))
+}
+
+
+func TestBalanced(t *testing.T) {
+	vals := []int{50, 25, 75, 30, 20, 70, 80, 35, 28}
+	var root *Node
+	for _, v := range vals {
+		root = Insert(root, v)
+	}
+
+	assert.Equal(t, true, IsBalanced(root))	
 }
